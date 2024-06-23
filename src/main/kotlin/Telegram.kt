fc060640cf85a812c47c3e5339bcf861508d7633
4meech <hornax@gmail.com>
2024-06-23 13:45:13 +0300

    Stage-VIII-2: repetitive URL-part is a constant now; botToken moved to TelegramBotService class constructor; creation of Regex instances is moved out of a while loop; getUpdates() and sendMessage()  methods are now using one common instance of HttpClient declared as private property "httpClient"

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -1,35 +1,35 @@
-fun main(args: Array<String> ) {
+fun main(args: Array<String>) {
 
-    val telegramBotService = TelegramBotService()
+    val telegramBotService = TelegramBotService(botToken = args[0])
 
-    val botToken = args[0]
     var updateId: Int? = 0
+
+    val updateIdRegex = "\"update_id\":(.+?),".toRegex()
+    val messageTextRegex = "\"text\":\"(.+?)\"".toRegex()
+    val chatIdRegex = "\"chat\":\\{\"id\":(.+?),".toRegex()
 
     while (true) {
         Thread.sleep(2000)
-        val updates: String = telegramBotService.getUpdates(botToken, updateId)
+        val updates: String = telegramBotService.getUpdates(updateId)
         println(updates)
 
-        val updateIdRegex = "\"update_id\":(.+?),".toRegex()
         val updateIdMatchResult: MatchResult? = updateIdRegex.find(updates)
         val idGroups = updateIdMatchResult?.groups
         updateId = idGroups?.get(1)?.value?.toInt()?.plus(1)
         println(updateId)
 
-        val messageTextRegex = "\"text\":\"(.+?)\"".toRegex()
         val textMatchResult: MatchResult? = messageTextRegex.find(updates)
         val textGroups = textMatchResult?.groups
         val text = textGroups?.get(1)?.value
         println(text)
 
-        val chatIdRegex = "\"chat\":\\{\"id\":(.+?),".toRegex()
         val chatIdMatchResult: MatchResult? = chatIdRegex.find(updates)
         val chatIdGroups = chatIdMatchResult?.groups
         val chatId = chatIdGroups?.get(1)?.value
         println(chatId)
 
         if (text != null && chatId != null) {
-            telegramBotService.sendMessage(botToken, chatId, text)
+            telegramBotService.sendMessage(chatId = chatId, message = text)
         }
     }
 }
